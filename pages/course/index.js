11929887dd3176237717028e484822a194cbdcae
Facundo Matteo <facumatteo@gmail.com>
2020-04-19 00:37:45 -0300

    Class 06 end

--- pages/course/index.js
+++ pages/course/index.js
@@ -1,23 +1,38 @@
-export default () => (
-  <>
-    <style jsx>{`
-      h2 {
-        margin-top: 24px;
-      }
-      section {
-        padding: 0 16px;
-      }
-    `}</style>
-    <section>
-      <h2>Codegnac.com Courses</h2>
-      <ul>
-        <li>
-          <a href="/course/javascript">JavaScript course</a>
-        </li>
-        <li>
-          <a href="/course/next">Next.js course</a>
-        </li>
-      </ul>
-    </section>
-  </>
-);
+import Link from "next/link";
+
+export default () => {
+  const username = "codegnac";
+
+  return (
+    <>
+      <style jsx>{`
+        h2 {
+          margin-top: 24px;
+        }
+        section {
+          padding: 0 16px;
+        }
+      `}</style>
+      <section>
+        <h2>Codegnac.com Courses</h2>
+        <ul>
+          <li>
+            <Link prefetch={false} href="/course/javascript">
+              <a>JavaScript course</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/course/next">
+              <a>Next.js course</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/users/[username]" as={`/users/${username}`}>
+              <a>Profile</a>
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
--- pages/users/[username].js
+++ pages/users/[username].js
@@ -1,1 +1,7 @@
-export default () => <div>I am the users page</div>;
+import { useRouter } from "next/router";
+
+export default () => {
+  const router = useRouter();
+  console.log(router.query);
+  return <div>I am the user {router.query.username}</div>;
+};
